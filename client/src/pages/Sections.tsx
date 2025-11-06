6761e1ed93d01d3307d51accdb78ff1ea3e40fed
omri <omrigamer100@gmail.com>
2025-11-06 16:11:37 +0200

    working on display sections

--- client/src/pages/Sections.tsx
+++ client/src/pages/Sections.tsx
@@ -1,9 +1,52 @@
-
+import { get_sections } from "../services/Videos"
+import { Box, Typography } from "@mui/material"
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 export const Sections = () => {
+    const [sections, setSections] = useState<video_sections[]>([])
+    const navigate = useNavigate()
+
+    interface video_sections {
+        id: number,
+        name: string
+        created_at: string
+    }
+
+    const fetch_sections = async () => {
+        try {
+            const response = await get_sections()
+
+            setSections(response.data)
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        fetch_sections()
+    }, [])
+
     return (
-        <div>
-            <h1>dispaly sesctions.</h1>
-        </div>
+        <Box sx={{ height: "90vh", display: "flex", flexDirection: "column", alignItems: "center", margin: "25px" }}>
+            <Typography sx={{ fontWeight: "bold", fontSize: "22px" }}>Video Sections</Typography>
+
+
+            <Box sx={{ backgroundColor: "black", width: "700px", height: "fit-content", marginTop: "15px", display: "flex", gap: 2, flexWrap: "wrap", padding: 2 }}>
+                {sections.map((item) => (
+
+                    <Box sx={{backgroundColor: "white", width: "200px", height: "200px"}}> 
+
+                        <Typography>{item.name}</Typography>
+
+                    </Box>
+
+                ))}
+            </Box>
+
+
+
+        </Box>
     )
 }
